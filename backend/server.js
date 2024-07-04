7f3a06832a23c43e4eb0906e01a79a492e750198
vaishnavikaleru <vaishnavikaleru9@gmail.com>
2024-07-05 02:46:17 +0530

    server_created

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,9 @@ const app=express();
 const dotenv=require('dotenv');
 const mongoose=require('mongoose');
 const cors=require('cors');
+
+
+
+app.listen(process.env.PORT || 2000,()=>{
+    console.log('connection established!');
+});

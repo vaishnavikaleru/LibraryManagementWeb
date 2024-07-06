3fddb179a47532f3c08320e20c12a64a30d1d170
vaishnavikaleru <vaishnavikaleru9@gmail.com>
2024-07-06 16:17:15 +0530

    signupstructurecreated

---
+++ frontend/src/signuppage/signup.js
@@ -0,0 +1,16 @@
+import signup  from './signup.css';
+import img from '../Images/book.png';
+import img2 from '../Images/Image2.jpg';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function SignUp()
+{
+    return (
+        <div>
+
+        </div>)
+
+}
+
+export default SignUp;

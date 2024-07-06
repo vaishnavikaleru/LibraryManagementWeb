2a4012187c492845703fd0d3e9164d614baced85
vaishnavikaleru <vaishnavikaleru9@gmail.com>
2024-07-06 15:10:00 +0530

    loginfunctionality

--- frontend/src/login/login.js
+++ frontend/src/login/login.js
@@ -2,3 +2,43 @@ import '../LoginPage/login.css';
 import img from '../Images/book.png';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+const url='http://localhost:2000/user/login';
+
+function login()
+{
+return (
+    <div>
+    <div className='firstbackground'>
+        <div id='navbar' className={scrolled?'scrolled':'noscroll'}>
+            <div className='logo'  onClick={()=>navigate('/')}>
+            <h1>Library Management</h1>
+            <img src={img} alt='book' width="50px" height="50px"/>
+            </div>
+                <nav>
+                    <h2>Price</h2>
+                    <h2>Support</h2>
+                    <h2>Contact Us</h2>
+                    <button name='login' onClick={()=>{navigate('/login');}}>Login</button>
+                    <button name='started' onClick={()=>navigate('/signup')}>Get Started</button>
+                </nav>
+            </div>
+            <div className='centerlogin'>
+               <div className='form'>
+                    <div className='headtitle'>
+                    <img src={img} width="70" height="70"/>
+                    <h2>Manager Login</h2>
+                    </div>
+                    <input type='email' name='email' className='email' placeholder='Email' onChange={(e)=>setemail(e.target.value)}/>
+                    <div className='password'><input type='password' name='password' placeholder='Password' onChange={(e)=>setpassword(e.target.value)}/>
+                    <h3>Forgot?</h3>
+                    </div>
+                    <button className='sign' onClick={()=>submitData()}>Sign In</button>
+                    <hr className='line'/>
+                    <h3 className='formfooter'>Don't have an account?<span onClick={()=>navigate('/signup')}>  Sign Up!</span></h3>
+               </div>
+            </div>
+    </div>
+
+    </div>
+    )
+}

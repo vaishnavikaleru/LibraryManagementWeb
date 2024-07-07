883bc6995aa43982da2e036724ed4ff413a2d658
vaishnavikaleru <vaishnavikaleru9@gmail.com>
2024-07-08 00:28:20 +0530

    loginroutesdefined

--- backend/Routes/registerroute.js
+++ backend/Routes/registerroute.js
@@ -3,6 +3,31 @@ const express=require('express');
 const router=express.Router();
 const jwt=require('jsonwebtoken');
 
+router.post('/login',async(req,res)=>{
+    const body=req.body;
+    const user=await model.find({email:body.email});
+    try
+    {
+        if(user)
+        {
+            if(user[0].password===body.password)
+            {
+                const token=jwt.sign({'ID':user[0]._id},process.env.JWT_SECRET);
+                res.json({login:true,message:'Successfully Login!','name':user[0].name,token:token});
+            }
+            else
+               res.json({login:false,message:'Password Incorrect'});
+            return;
+        }
+        else
+        res.json({login:false,message:'User not found!'});
+    }
+    catch(exp)
+    {
+        res.json({login:false,message:'Error occuring in login!'});
+    }
+   
+});
 
 router.post('/register',async(req,res)=>{
     const body=req.body;

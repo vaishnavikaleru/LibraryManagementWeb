996a8deb7990408bd8a2dbe179b15dfea65c0b5a
vaishnavikaleru <vaishnavikaleru9@gmail.com>
2024-07-08 00:21:49 +0530

    subpage2.js

--- frontend/src/AddItemPage/additempage.js
+++ frontend/src/AddItemPage/additempage.js
@@ -9,7 +9,6 @@ import { useEffect, useState } from 'react';
 function Item()
 {
     return(
-    <div>
     <div className='Page'>
     <div className='subpage1'>
         <div className='logo'>
@@ -29,6 +28,50 @@ function Item()
             <h2 onClick={()=>logout()}>Log Out</h2>
             <button>Upgrade</button>
         </div>
+        </div>
+        <div className='subpage2'>
+          <div className='searchbar'>
+            <div className='search'>
+              <h1>Add Your Collection</h1>
+            </div>
+            <div className='logout'>
+               <h3>username</h3>
+               <img src={btn} width='50' height='50'/>
+            </div>
+          </div>
+          <hr/>
+        <div className='collectionform'>
+            <div className='bar1'>
+                <h2>Selection Collection</h2>
+                <select>
+                   {
+                   collectionname.map((e,index)=>(
+                    <option key={index}>{e}</option>
+                   )
+                  )}
+                </select>
+                <h4>Choose the collection you're adding items to.</h4>
+            </div>
+            <div className='bar2'>
+                <h2>Selection Item Type</h2>
+                <select>
+                   <option>Book</option>
+                   <option>Movie</option>
+                   <option>Music</option>
+                   <option>Video Game</option>
+                </select>
+                <h4>The type of item are you adding.</h4>
+            </div>
+            <div className='bar3'>
+                <h2>Search for Books </h2>
+                <input type='text' placeholder='Search'/>
+                <h4>Search by ISBN or keyword. ISBN search will auto-add an item.</h4>
+            </div>
+         
+            <div className='results'>
+            <hr/>
+               <h1>Results</h1>
+            </div>
         </div>
         </div>
         </div>

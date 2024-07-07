5ba1752a8096ad86f35ee2adbd0601307d2895a6
vaishnavikaleru <vaishnavikaleru9@gmail.com>
2024-07-07 15:22:24 +0530

    collectionsubpage

--- frontend/src/AddCollectionPage/addcollectionpage.js
+++ frontend/src/AddCollectionPage/addcollectionpage.js
@@ -27,6 +27,37 @@ function Collection()
                 <button>Upgrade</button>
             </div>
             </div>
+            <div className='subpage2'>
+          <div className='searchbar'>
+            <div className='search'>
+              <h1>Add Your Collection</h1>
+            </div>
+            <div className='logout'>
+               <h3>username</h3>
+               <img src={btn} width='50' height='50'/>
+            </div>
+          </div>
+          <hr/>
+        <div className='collectionform'>
+            <div className='bar1'>
+                <h2>Collection Title</h2>
+                <input type='text' placeholder='Collection Title' onChange={(e)=>setCollectionName({...collectionName,collectionname:e.target.value})}/>
+                <h4>Limit 40 characters. e.g. (My Books, Movie Wishlist, Console Games, Family CD Collection).</h4>
+            </div>
+            <div className='bar2'>
+                <h2>Language</h2>
+                <select onChange={(e)=>setCollectionName({...collectionName,language:e.target.value})}>
+                    <option>Arabic</option>
+                    <option>Bengali</option>
+                    <option>Dutch</option>
+                    <option>English</option>
+                    <option>Hindi</option>
+                </select>
+                <h4>The display language of the items in this collection. Determines how best to sort alphabetically.</h4>
+            </div>
+            <button onClick={()=>updateCollection()}>Add Collection</button>
+        </div>
+        </div>
             </div>
     )
 }

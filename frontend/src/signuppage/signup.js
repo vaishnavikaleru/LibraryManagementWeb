32f6d569ccfb962b198dfa7d9301a88e20a29d64
vaishnavikaleru <vaishnavikaleru9@gmail.com>
2024-07-07 14:16:00 +0530

    singuppagediv

--- frontend/src/signuppage/signup.js
+++ frontend/src/signuppage/signup.js
@@ -9,6 +9,20 @@ function SignUp()
     return (
         <div>
 
+        <div className='firstbackground'>
+            <div id='navbar' className={scrolled?'scrolled':'noscroll'}>
+                <div className='logo'  onClick={()=>navigate('/')}>
+                <h1>Library Management</h1>
+                <img src={img} alt='book' width="50px" height="50px"/>
+                </div>
+                    <nav>
+                        <h2>Price</h2>
+                        <h2>Support</h2>
+                        <h2>Contact Us</h2>
+                        <button name='login' onClick={()=>{navigate('/login');}}>Login</button>
+                        <button name='started' onClick={()=>navigate('/signup')}>Get Started</button>
+                    </nav>
+                </div>
         </div>)
 
 }
